b5c699cd0e01e541c2fb2775cfa8c40a7cc801e5
GabyQueiroz <gabriellyp@alunos.utfpr.edu.br>
2026-08-24 16:25:13 -0300

    Export vector store count helper

--- src/rag/__init__.py
+++ src/rag/__init__.py
@@ -1,6 +1,6 @@
 from .doc_loader import load_docs
 from .chunker import chunk_data
-from .vector_store import get_vector_store
+from .vector_store import get_vector_store, get_vector_store_document_count
 from .embeddings import get_embeddings
 from .llm import ask_question
 from .retriever import retrieve
@@ -11,6 +11,7 @@ __all__ = [
     "load_docs",
     "chunk_data",
     "get_vector_store",
+    "get_vector_store_document_count",
     "get_embeddings",
     "ask_question",
     "retrieve",
